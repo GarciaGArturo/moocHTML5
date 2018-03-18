d511f60b535f8dc435d382842f0937229f4db03b
rumpmx <rumpeltzinky@gmail.com>
2018-03-18 17:48:23 -0600

    correccion

--- mod_08/cronometro.js
+++ mod_08/cronometro.js
@@ -8,9 +8,11 @@ function addZero(n)  {if (+n < 10) n = ("0"+ n); return n; };
 function incr()     { localStorage.c = +localStorage.c + 0.01; mnts();};
 function mostrar()  { cl.html(minut + ":" + addZero((+localStorage.c).toFixed(2)) ); };
 function arrancar() { t=setInterval(function(){incr(); mostrar()}, 10); };
-function parar()    { clearInterval(t);  t=undefined; inf.html("&nbsp;"); };
+function parar()    { clearInterval(t);  t=undefined; inf.html("&nbsp;"); listado(); };
 function cambiar()  { inf.html("&nbsp;"); if (!t) arrancar(); else parar(); };
 
+
+function listado()  { instante.html(instante.html() + ("<p>Parado en " + cl.html() + " seg.</p>")); };
 function inicializa() { cl.html("0:00.00"); instante.html(""); inf.html("&nbsp;"); localStorage.c="0.00"; minut = 0;  mostrar(); };
 
 if ("ontouchstart" in document.documentElement){
